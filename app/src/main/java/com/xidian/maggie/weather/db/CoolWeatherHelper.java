3e77eeee3415b3c03be8a658d124810fc3789052
Maggie <maggie-huang@hotmail.com>
2015-07-22 12:42:55 +0800

    completed the database

---
+++ app/src/main/java/com/xidian/maggie/weather/db/CoolWeatherHelper.java
@@ -0,0 +1,42 @@
+package com.xidian.maggie.weather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by maggie on 2015/7/22.
+ */
+public class CoolWeatherHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_PROVINCE = "create table Province("
+            + "id integer primary key autoincrement,"
+            + "province_name text,"
+            + "province_code text)";
+    public static final String CREATE_CITY = "create table city("
+            + "id integer primary key autoincrement,"
+            + "city_name text,"
+            + "city_code text,"
+            + "province_id integer)";
+    public static final String CREATE_COUNTY = "create table county("
+            + "id integer primary key autoincrement,"
+            + "county_name text,"
+            + "county_code text,"
+            + "city_id integer)";
+
+    public CoolWeatherHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
